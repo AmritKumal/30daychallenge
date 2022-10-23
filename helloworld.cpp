cac5ec57cdb469474c95375183ab6b722234e1ce
Amrit Kumal <amritkumal93@gmail.com>
2022-10-23 21:04:47 +0545

    day 1

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,3 +7,10 @@ int main()
     return 0;
 }
 // ctrl+ z y
+// echo "# 30daychallenge" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/AmritKumal/30daychallenge.git
+// git push -u origin main
