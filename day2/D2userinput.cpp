c90ed22b9ba1ab6a6349cf0d53ff31e3713bdf18
Amrit Kumal <amritkumal93@gmail.com>
2022-10-24 22:03:33 +0545

    day 2

---
+++ day2/D2userinput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    int sum;
+    cout << "enter the value";
+    cin >> x >> y;
+
+    sum = x + y;
+    cout << "sum is" << sum;
+    return 0;
+}
---
+++ day2/D2variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string myname = " Amrit Kumal ";
+    int age = 22;
+    string address = " kathmandu ";
+    cout << "my name is " << myname << endl;
+    cout << " my name is " << myname << "i live in " << address << " i am " << age << "years old";
+    return 0;
+}
