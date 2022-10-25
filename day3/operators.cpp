dbb0c335290173dc8f79f2bb85b05e62f246a7c4
Amrit Kumal <amritkumal93@gmail.com>
2022-10-25 20:30:30 +0545

    day 3

---
+++ day3/operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 5, y = 9;
+    ++x;
+
+    cout << "increment is " << x << endl;
+    cout << " remainder is " << y % 4;
+
+    return 0;
+}
