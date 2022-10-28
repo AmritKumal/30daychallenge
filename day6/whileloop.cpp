bb39b3f20d45485bd0d7b47b7ed9dce756c01583
Amrit Kumal <amritkumal93@gmail.com>
2022-10-28 21:32:17 +0545

    day 6

---
+++ day6/whileloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    while (i < 5)
+    {
+        i++;
+        cout << " numbers are " << i << endl;
+    }
+
+    return 0;
+}
