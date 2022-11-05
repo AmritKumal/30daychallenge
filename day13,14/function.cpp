a6db275a5983c3da619f9bfa82cfbd2ba5f7a856
Amrit Kumal <amritkumal93@gmail.com>
2022-11-05 21:23:14 +0545

    day13,14

---
+++ day13,14/function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void myfunction()
+{
+    cout << " my name is amrit " << endl;
+}
+
+int main()
+{
+    myfunction();
+    myfunction();
+    myfunction();
+
+    return 0;
+}
